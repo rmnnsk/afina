8609b734fce7d0b1b96aaa8f76d38affa6f65faa
rmnnsk <roman08oskin@gmail.com>
2021-02-16 15:06:41 +0300

    First LRU realization, based on list+map structure

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -1,22 +1,146 @@
+#include <iostream>
+#include <limits>
+#include <memory>
+
 #include "SimpleLRU.h"
 
 namespace Afina {
 namespace Backend {
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Put(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::Put(const std::string &key, const std::string &value) {
+    // Check if pair size more than max value of type
+    if (std::numeric_limits<size_t>::max() - key.size() < value.size()) { // maybe decltype?
+        return false;
+    }
+
+    // Check if pair size more than max size of cache
+    if (this->_max_size < value.size() + key.size()) {
+        return false;
+    }
+
+    // Delete pairs from queue while we can't insert new pair.
+    auto free_queue_space = [&](size_t need_space) {
+        while (_cur_free < need_space) {
+            if (!this->_lru_last) {
+                break;
+            }
+            _cur_free += this->_lru_last->key.size() + this->_lru_last->value.size();
+
+            // 1. Delete from map
+            // I think it's more profitable to delete pair from map, even if needed key was already there.
+            this->_lru_index.erase(std::cref(this->_lru_last->key));
+
+            // 2. Delete from queue
+            this->_lru_last = this->_lru_last->prev;
+            if (this->_lru_last) {
+                this->_lru_last->next = nullptr;
+            }
+        }
+    };
+
+    // Check if key already in lru_index
+    // If it's inside, then move it to head, free space, change value.
+    // Else, simply freeing space and insert pair in head.
+
+    auto key_wrapper = std::cref(key);
+
+    auto node_it = this->_lru_index.find(key);
+
+    if (node_it != this->_lru_index.end()) {
+        auto cur_node = node_it->second;
+        std::unique_ptr<lru_node> cur_uniq;
+        if (this->_lru_head->key != key) {
+            cur_uniq = std::move(cur_node.get().prev->next);
+            // Delete element from it's position
+            if (cur_node.get().next) {
+                cur_node.get().next->prev = cur_node.get().prev;
+            }
+            else {
+                // If delete from end, then set new lru_last
+                this->_lru_last = cur_node.get().prev;
+            }
+            cur_node.get().prev->next = std::move(cur_node.get().next);
+            this->_lru_head->prev = cur_uniq.get();
+            cur_uniq->next = std::move(this->_lru_head);
+            cur_uniq->prev = nullptr;
+            this->_lru_head = std::move(cur_uniq);
+        }
+        size_t need_space =
+            (value.size() < this->_lru_head->value.size()) ? 0 : (value.size() - this->_lru_head->value.size());
+        free_queue_space(need_space);
+        cur_node.get().value = value;
+        this->_cur_free -= need_space;
+    } else {
+        //        std::cerr << "KEY NOT IN MAP" << std::endl;
+        size_t need_space = (key.size() + value.size());
+        free_queue_space(need_space);
+        auto new_node = std::unique_ptr<lru_node>(new lru_node(key, value));
+        if (this->_lru_head) {
+            this->_lru_head->prev = new_node.get();
+            new_node->next = std::move(this->_lru_head);
+        }
+        else {
+            this->_lru_last = new_node.get();
+        }
+        this->_lru_head = std::move(new_node);
+        this->_lru_index.emplace(std::ref(_lru_head->key), std::reference_wrapper<lru_node>(*_lru_head));
+        this->_cur_free -= need_space;
+    }
+    return true;
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::PutIfAbsent(const std::string &key, const std::string &value) {
+    auto key_wrapper = std::cref(key);
+    if (this->_lru_index.find(key_wrapper) != this->_lru_index.end()) {
+        return false;
+    }
+    return this->Put(key, value);
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Set(const std::string &key, const std::string &value) { return false; }
+bool SimpleLRU::Set(const std::string &key, const std::string &value) {
+    auto key_wrapper = std::cref(key);
+    if (this->_lru_index.find(key_wrapper) == this->_lru_index.end()) {
+        return false;
+    }
+    return this->Put(key, value);
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Delete(const std::string &key) { return false; }
+bool SimpleLRU::Delete(const std::string &key) {
+    auto key_wrapper = std::cref(key);
+
+    auto node_it = this->_lru_index.find(key);
+    if (node_it == this->_lru_index.end()) {
+        return false;
+    }
+    auto cur_node = node_it->second;
+    this->_lru_index.erase(key_wrapper);
+    if (this->_lru_head->key == key) {
+        this->_lru_head = std::move(this->_lru_head->next);
+        this->_lru_head->prev = nullptr;
+    } else {
+        if (cur_node.get().next) {
+            cur_node.get().next->prev = cur_node.get().prev;
+        }
+        cur_node.get().prev->next = std::move(cur_node.get().next);
+    }
+    return true;
+}
 
 // See MapBasedGlobalLockImpl.h
-bool SimpleLRU::Get(const std::string &key, std::string &value) { return false; }
+bool SimpleLRU::Get(const std::string &key, std::string &value) {
+    auto key_wrapper = std::cref(key);
+    auto node_it = this->_lru_index.find(key_wrapper);
+    if (node_it == this->_lru_index.end()) {
+        return false;
+    }
+    value = node_it->second.get().value;
+    bool hitted = this->Put(key, value);
+    return hitted;
+}
 
 } // namespace Backend
 } // namespace Afina
--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <utility>
 
 #include <afina/Storage.h>
 
@@ -17,11 +18,13 @@ namespace Backend {
  */
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) : _max_size(max_size) {}
+    explicit SimpleLRU(size_t max_size = 1024) : _max_size(max_size), _cur_free(max_size) {}
 
-    ~SimpleLRU() {
+    ~SimpleLRU() override {
         _lru_index.clear();
-        _lru_head.reset(); // TODO: Here is stack overflow
+        while (this->_lru_head) {
+            this->_lru_head = std::move(this->_lru_head->next);
+        }
     }
 
     // Implements Afina::Storage interface
@@ -44,22 +47,33 @@ private:
     using lru_node = struct lru_node {
         std::string key;
         std::string value;
-        std::unique_ptr<lru_node> prev;
+        lru_node *prev = nullptr;
         std::unique_ptr<lru_node> next;
+
+        lru_node() = default;
+        lru_node(std::string key, std::string value) : key(std::move(key)), value(std::move(value))
+        {};
     };
 
     // Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be less the _max_size
     std::size_t _max_size;
 
+    // Current value of free bytes in cache.
+    std::size_t _cur_free;
+
     // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
     // element that wasn't used for longest time.
     //
     // List owns all nodes
-    std::unique_ptr<lru_node> _lru_head;
+    std::unique_ptr<lru_node> _lru_head = nullptr;
+
+    // Ptr to last element of nodes list
+    lru_node *_lru_last = nullptr;
 
     // Index of nodes from list above, allows fast random access to elements by lru_node#key
-    std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
+    std::map<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>, std::less<std::string>>
+        _lru_index;
 };
 
 } // namespace Backend
